b71f53ab2953dbcbe24586c5cd4c2b840b591215
jaebin1234 <jaebin2586@gmail.com>
2020-06-30 13:47:57 +0900

    부스트코스_4일차

---
+++ src/pack/TodayServlet.java
@@ -0,0 +1,33 @@
+package pack;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "TodayServlet")
+public class TodayServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+                response.setContentType("text/html;charset=utf-8");
+                PrintWriter out = response.getWriter();
+                Date today = new Date();
+                out.println("<html>" +
+                        "<body>" +
+                        "<h1>메인화면</h1><br>" +
+                        "<br>" + "현재시각 : " + today + "</body></html>");
+
+
+
+        }
+
+    }
+
